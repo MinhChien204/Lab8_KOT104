f180b9d442a4695f840127e1ff3efe382736dbe6
MinhChien204 <122662541+MinhChien204@users.noreply.github.com>
2024-06-10 23:53:18 +0700

    lab 8 done

---
+++ app/src/main/java/com/example/kot104/service/RetrofitService.kt
@@ -0,0 +1,14 @@
+package com.example.kot104.service
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+open class RetrofitService {
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl("https://652670da917d673fd76c444b.mockapi.io/api/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    val movieService: MovieService by lazy {
+        retrofit.create(MovieService::class.java)
+    }
+}
